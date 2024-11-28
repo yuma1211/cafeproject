9d7c96afd11a2c97eedfbaa2a2eb02d4a70c28f5
yuma <mit2471537@stu.o-hara.ac.jp>
2024-11-28 17:35:52 +0900

    local_settings.pyを追加

--- cafeproject/settings.py
+++ cafeproject/settings.py
@@ -149,3 +149,9 @@ EMAIL_PORT=587
 EMAIL_HOST_USER='lingmuhucai6@gmail.com'
 EMAIL_HOST_PASSWORD='kngv xlbs yuve qccp'
 EMAIL_USE_TLS=True
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
